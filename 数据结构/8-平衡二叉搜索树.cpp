731b7aaa7a1d3c347247cbb27950987d5aa7c145
yinheyi <chinayinheyi@163.com>
2019-05-26 22:26:29 +0800

    增加平衡二叉搜索树与红黑树空文件

---
+++ 数据结构/8-平衡二叉搜索树.cpp
@@ -0,0 +1,34 @@
+/***********************************************************************
+*   Copyright (C) 2019  Yinheyi. <chinayinheyi@163.com>
+*   
+* This program is free software; you can redistribute it and/or modify it under the terms
+* of the GNU General Public License as published by the Free Software Foundation; either 
+* version 2 of the License, or (at your option) any later version.
+
+*   Brief:    
+*   Author: yinheyi
+*   Email: chinayinheyi@163.com
+*   Version: 1.0
+*   Created Time: 2019年05月18日 星期六 16时38分08秒
+*   Modifed Time: 2019年05月26日 星期日 22时24分48秒
+*   Blog: http://www.cnblogs.com/yinheyi
+*   Github: https://github.com/yinheyi
+*   
+***********************************************************************/
+#include <iostream>
+
+
+// 1.平衡二叉搜索树在二叉搜索树的基础上增加了一个限制条件：要求左右子树的高度差不得大于1.
+// 2.平衡二叉搜索树又称作AVL树,AVL是它的两个发明者的名字中的字母。
+// 3.平衡二叉搜索树的难点在于当二叉树不满足限制条件时，如何调整树的结构, 这里就涉及到结点
+// 的左旋和右旋问题。
+// 4.对于平衡二叉搜索树的结点来说，需要在二叉搜索树的基础上增加一个表示左右子树高度差的值，
+// 
+//
+
+/**********************    测试程序     *************************/
+int main(int argc, char* argv[])
+{
+	return 0;
+}
+
---
+++ 数据结构/9-红黑树.cpp
@@ -0,0 +1,26 @@
+/***********************************************************************
+*   Copyright (C) 2019  Yinheyi. <chinayinheyi@163.com>
+*   
+* This program is free software; you can redistribute it and/or modify it under the terms
+* of the GNU General Public License as published by the Free Software Foundation; either 
+* version 2 of the License, or (at your option) any later version.
+
+*   Brief:    
+*   Author: yinheyi
+*   Email: chinayinheyi@163.com
+*   Version: 1.0
+*   Created Time: 2019年05月18日 星期六 16时38分00秒
+*   Modifed Time: 2019年05月18日 星期六 16时38分04秒
+*   Blog: http://www.cnblogs.com/yinheyi
+*   Github: https://github.com/yinheyi
+*   
+***********************************************************************/
+#include <iostream>
+
+
+/**********************    测试程序     *************************/
+int main(int argc, char* argv[])
+{
+	return 0;
+}
+
