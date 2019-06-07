923662103958d9733d09ad796f9820ed1446e70c
殷和义 <1140195102@qq.com>
2019-06-07 15:03:34 +0800

    Update 9-红黑树.cpp
    增加红黑树结点的结构体和红黑树类的定义。

--- 数据结构/9-红黑树.cpp
+++ 数据结构/9-红黑树.cpp
@@ -18,6 +18,62 @@
 #include <iostream>
 
 
+// 定义一个枚举类型，表示红黑树的颜色
+ enum RBColor
+{
+	RED,
+	BLACK,
+}
+
+// 红黑树结点的定义
+struct RBNode
+{
+	RBNode();
+	int m_nValue;
+	int m_nColor;
+	RBNode* m_pParent;
+	RBNode* m_pLeft;
+	RBNode* m_pRigh;
+};
+
+// 红黑树结点的构造函数
+RBNode::RBNode()
+{
+	m_nValue = 0;
+	m_nColor = RED;
+	m_pParent = nullptr;
+	m_npLeft = nullptr;
+	m_npRight = nullptr;
+}
+
+// 红黑树类的定义
+class RBTree
+{
+public:
+	RBTree();
+	~RBTree();
+	bool insert(int nValue_);
+	bool erase(RBNode* pNode_);
+	size_t size();
+	bool empty();
+	int min() const;
+	int max() const;
+	
+protected:
+	void Transplant(RBNode* pOldNode_, RBNode* pNewNode_);		// 以新树代替旧树
+	RBNode* precursor(RBNode* pNode_);	// 求前驱结点
+	RBNode* successor(RBNode* pNode_);	// 求后驱结点
+	RBNode* min_node(RBNode* pNode_);	// 查找给定子树中最小结点的指针
+	RBNode* max_node(RBNode* pNode_);	// 查找给定子树中最大结点的指针
+	void free_memory(RBNode* pNode);	// 释放给定红黑树的内存空间
+private:
+	size_t m_nSize;
+	RBNode* m_pRoot;
+};
+
+// 
+}
+
 /**********************    测试程序     *************************/
 int main(int argc, char* argv[])
 {
